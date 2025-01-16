9da263fc5ac05f27a58666fc93db9d645c43572d
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-01-16 19:54:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	
+   let ans = 0;
+
+  arr.sort((a, b) => a - b);
+  while (arr.length > 1) {
+
+    let first = arr.shift();
+    let second = arr.shift();
+
+    let total = first + second;
+    ans = ans + total;
+    arr.push(total);
+    arr.sort((a, b) => a - b);
+  }
+  return ans;
 }
 
 module.exports=mincost;
